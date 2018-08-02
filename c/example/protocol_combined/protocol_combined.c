e979a2e5c3addb9cb1dc5969d5b57d1a9f96ea1b
jimmy hu <60071jimmy@gmail.com>
2018-08-02 14:52:24 -0400

    Move kbhit function
    Move kbhit function

--- c/example/protocol_combined/protocol_combined.c
+++ c/example/protocol_combined/protocol_combined.c
@@ -78,38 +78,7 @@
 
 // Function declaration
 int getch(void);
-
-
-int kbhit(void)
-{
-#if defined(__linux__) || defined(__APPLE__)
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
-
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if (ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
-#elif defined(_WIN32) || defined(_WIN64)
-  return _kbhit();
-#endif
-}
+int kbhit(void);
 
 int main()
 {
